cbe7f731ea5c45de78cf8f50d13ed30cf642c918
Berk Onder <34274686+OnderBerk@users.noreply.github.com>
2020-02-17 14:16:41 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcdatabase.MvcApplication" Language="C#" %>
---
+++ Views/ViewModels/Home/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using mvcdatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcdatabase.Views.ViewModels.Home
+{
+    public class HomePageViewModel
+    {
+        public List<Kisiler> Kisiler { get; set; }
+        public List<Adresler> Adresler { get; set; }
+    }
+}
